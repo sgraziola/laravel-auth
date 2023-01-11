992bbf2efefb818a6214ca664bbd92c1b0fb6e1d
sgraziola <simo.fdl@gmail.com>
2023-01-11 10:28:08 +0100

    Add slug update

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -79,6 +79,8 @@ class ProjectController extends Controller
     public function update(UpdateProjectRequest $request, Project $project)
     {
         $val_data = $request->validated();
+        $project_slug = Project::generateSlug($val_data['title']);
+        $val_data['slug'] = $project_slug;
         $project->update($val_data);
 
         return to_route('admin.projects.index')->with('message', "$project->title updated successfully");
