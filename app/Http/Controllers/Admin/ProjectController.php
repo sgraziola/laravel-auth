5fad65ec6b5c3e2d299fe733668122096c757850
sgraziola <simo.fdl@gmail.com>
2023-01-13 15:05:05 +0100

    add if statement in destroy func

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -117,6 +117,10 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        if ($project->thumb) {
+            Storage::delete($project->thumb);
+        }
+
         $project->delete();
 
         return to_route('admin.projects.index')->with('message', "$project->title deleted successfully");
